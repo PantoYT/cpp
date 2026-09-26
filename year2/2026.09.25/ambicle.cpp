ebe46f26d300f03bd585273f7e2d66bc4e87a1b3
PantoYT <halasawojciech@gmail.com>
2026-09-26 15:54:23 +0200

    ambicle

--- year2/2026.09.25/ambicle.cpp
+++ year2/2026.09.25/ambicle.cpp
@@ -1,48 +1,39 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int suma_dziel_wlasc(int n) {
+int sumator(int n) {
     int suma = 0;
-
     for (int i=1;i<n;i++) {
         if (n%i==0) {
             suma+=i;
         }
     }
-
     return suma;
 }
 
 int zaprzyjaznione(int n) {
-    int i = 1;
-    int licznik = 0;
-    int smol = 0;
-
-    while (licznik < n) {
-        int suma1 = suma_dziel_wlasc(i);
-        if (i < suma1) {
-            int suma2 = suma_dziel_wlasc(suma1);
-
-            if (suma2 == i) {
-                licznik++;
-                smol = i;
-            }
+    int licznik,suma1,suma2,smol;
+    licznik=suma1=suma2=smol=0;
+    int i=1;
+    while(licznik<n) {
+        suma1 = sumator(i);
+        suma2 = sumator(suma1);
+        if (i == suma2 && i < suma1) {
+            licznik++;
+            smol = i;
         }
         i++;
     }
     return smol;
 }
 
-int main()
+int main() 
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-
     int n;
     cin>>n;
-
     cout<<zaprzyjaznione(n);
     return 0;
 }
